6c66afe2f1b9fb528f2632b35d8d8424c3a03620
nwakalordivine <johndoe@example.com>
2025-05-15 01:35:35 +0100

    fix ALLOWED_HOSTS

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['.onrender.com']
+ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1/']
 
 # Application definition
 
